1e70f9b488c1f5b35587ae8ba63af39261315f8a
Roni Laukkarinen <roni@dude.fi>
2020-03-25 18:52:29 +0200

    Page head

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -5,7 +5,7 @@
  * @Author: Roni Laukkarinen
  * @Date: 2020-02-28 15:38:00
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2020-03-25 18:47:13
+ * @Last Modified time: 2020-03-25 18:52:18
  * @package minimalistmadness
  */
 
@@ -13,7 +13,7 @@ namespace Air_Light;
 ?>
 
 <section class="block block-hero">
-  <div class="container">
-    <h1>Hei, olen Rolle ja tämä on blogini. Rollemaa sisältää yhteensä <?php echo esc_attr( wp_count_posts()->publish ); ?> kirjoitusta, jotka pitävät sisällään <?php echo post_word_count_by_author(); ?> sanaa.</h1>
+  <div class="page-head">
+    <h1>Olen Rolle ja tämä on blogini, joka sisältää <?php echo esc_attr( wp_count_posts()->publish ); ?> kirjoitusta, eli <?php echo post_word_count_by_author(); ?> sanaa.</h1>
   </div>
 </section>
