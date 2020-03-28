439eb793976af7cf68e04eff3758ff9ce7870bb2
Roni Laukkarinen <roni@dude.fi>
2020-03-28 19:42:19 +0200

    Fix load more height and link

--- template-parts/who.php
+++ template-parts/who.php
@@ -20,21 +20,21 @@ namespace Air_Light;
         </div>
 
         <div>
-          <p><a href="<?php get_page_link( 2768 ); ?>">Lue lisää <svg fill="currentColor" width="9" height="9" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45L531 45q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"/></svg></a></p>
+          <p><a href="<?php echo esc_url( get_page_link( 2768 ) ); ?>">Lue lisää <svg fill="currentColor" width="9" height="9" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45L531 45q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"/></svg></a></p>
         </div>
       </div>
       <div class="col col-images">
-        <div class="instagram instagram-1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/instagram-5.jpg');"><a href="https://www.instagram.com/p/_P_FY3G0Vm" class="global-link"></a></div>
+        <div class="instagram instagram-1" style="background-image: url('<?php echo esc_url( esc_url( get_template_directory_uri() ) ); ?>/images/instagram-5.jpg');"><a href="https://www.instagram.com/p/_P_FY3G0Vm" class="global-link"></a></div>
       </div>
 
       <div class="col col-images-multiple">
-        <div class="instagram instagram-1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/instagram-2.jpg');"><a href="https://www.instagram.com/p/BWkzo1YH41w" class="global-link"></a></div>
+        <div class="instagram instagram-1" style="background-image: url('<?php echo esc_url( get_template_directory_uri() ); ?>/images/instagram-2.jpg');"><a href="https://www.instagram.com/p/BWkzo1YH41w" class="global-link"></a></div>
 
-        <div class="instagram instagram-1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/instagram-3.jpg');"><a href="https://www.instagram.com/p/BkvleS7HJWF" class="global-link"></a></div>
+        <div class="instagram instagram-1" style="background-image: url('<?php echo esc_url( get_template_directory_uri() ); ?>/images/instagram-3.jpg');"><a href="https://www.instagram.com/p/BkvleS7HJWF" class="global-link"></a></div>
 
-        <div class="instagram instagram-1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/instagram-4.jpg');"><a href="https://www.instagram.com/p/BD0kbXIm0T3" class="global-link"></a></div>
+        <div class="instagram instagram-1" style="background-image: url('<?php echo esc_url( get_template_directory_uri() ); ?>/images/instagram-4.jpg');"><a href="https://www.instagram.com/p/BD0kbXIm0T3" class="global-link"></a></div>
 
-        <div class="instagram instagram-1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/instagram-1.jpg');"><a href="https://www.instagram.com/p/BNL6xgrg3iR" class="global-link"></a></div>
+        <div class="instagram instagram-1" style="background-image: url('<?php echo esc_url( get_template_directory_uri() ); ?>/images/instagram-1.jpg');"><a href="https://www.instagram.com/p/BNL6xgrg3iR" class="global-link"></a></div>
 
       </div>
     </div>
