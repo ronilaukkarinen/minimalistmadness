bfb6580fc6a5adcafbf2731dae89da2f99325994
Roni Laukkarinen <roni@dude.fi>
2021-07-22 11:12:42 +0300

    More accessible archive links

--- page-all.php
+++ page-all.php
@@ -44,7 +44,7 @@ get_header(); ?>
             ?>
 
             <li>
-              <a href="<?php echo esc_url( get_the_permalink() ); ?>" class="global-link"><span class="screen-reader-text"><?php echo esc_attr( get_the_title() ); ?></span></a>
+              <a href="<?php echo esc_url( get_the_permalink() ); ?>" class="global-link" aria-hidden="true" tabindex="-1"></a>
               <p class="post-card-details"><time datetime="<?php the_time( 'c' ); ?>"><?php the_time( 'j.' ); ?> <?php the_time( 'F' ); ?>ta <?php the_time( 'Y' ); ?></time></p>
               <h3 class="post-card-title"><a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php echo esc_attr( get_the_title() ); ?></a></h3>
             </li>
