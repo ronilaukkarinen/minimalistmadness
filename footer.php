b09dbcce93e6f350d58c6ef6064673bc6a4302da
Roni Laukkarinen <roni@dude.fi>
2021-07-22 17:58:58 +0300

    Add goodreads, optimize svgs

--- footer.php
+++ footer.php
@@ -35,6 +35,7 @@ namespace Air_Light;
           <li><a class="no-external-link-indicator" href="https://profiles.wordpress.org/rolle"><span class="screen-reader-text">WordPress</span><?php include get_theme_file_path( '/svg/wordpress.svg' ); ?></a></li>
           <li><a class="no-external-link-indicator" href="https://www.linkedin.com/in/rolaukka/"><span class="screen-reader-text">WordPress</span><?php include get_theme_file_path( '/svg/linkedin.svg' ); ?></a></li>
           <li><a class="no-external-link-indicator" href="https://www.strava.com/athletes/16582440"><span class="screen-reader-text">Strava</span><?php include get_theme_file_path( '/svg/strava.svg' ); ?></a></li>
+          <li><a class="no-external-link-indicator" href="https://goodreads.com/rolle"><span class="screen-reader-text">Goodreads</span><?php include get_theme_file_path( '/svg/goodreads.svg' ); ?></a></li>
         </ul>
 
         <p>Oikeudet omistaa Roni Laukkarinen, 1999-<?php echo esc_attr( date( 'Y' ) ); ?>.</p>
