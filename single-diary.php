88d07b2cbf8beb14d3742abf6ebd6514b0eaece0
Roni Laukkarinen <roni@dude.fi>
2021-11-11 23:35:27 +0200

    Remove heatmap from single diary article

--- single-diary.php
+++ single-diary.php
@@ -15,7 +15,6 @@ get_header();
 <div id="primary" class="content-area">
   <main id="main" class="site-main">
 
-    <?php get_template_part( 'template-parts/heatmap' ); ?>
     <?php while ( have_posts() ) :
     the_post();
       get_template_part( 'template-parts/content-diary' );
