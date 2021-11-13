cd186dd000f8d2be5ec34c933bd20b76a70a31c8
Roni Laukkarinen <roni@dude.fi>
2021-11-14 01:00:54 +0200

    Add diary posts to total post count

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -5,7 +5,7 @@
  * @Author: Roni Laukkarinen
  * @Date: 2020-02-28 15:38:00
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-09-07 13:34:34
+ * @Last Modified time: 2021-11-14 01:00:37
  * @package minimalistmadness
  */
 
@@ -15,6 +15,6 @@ namespace Air_Light;
 
 <section class="block block-hero">
   <div class="page-head">
-    <h1>Rollemaa on Rollen blogi, joka sisältää <?php echo esc_attr( wp_count_posts()->publish ); ?> kirjoitusta, eli <?php echo esc_html( get_word_count_from_posts() ); ?> sanaa.</h1>
+    <h1>Rollemaa on Rollen blogi, joka sisältää <?php echo esc_attr( wp_count_posts( 'post' )->publish + wp_count_posts( 'diary' )->publish ); ?> kirjoitusta, eli <?php echo esc_html( get_word_count_from_posts() ); ?> sanaa.</h1>
   </div>
 </section>
