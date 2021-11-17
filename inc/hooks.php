0b8a756340f7839f32c99b1008ee7c9fffb66487
Roni Laukkarinen <roni@dude.fi>
2021-11-17 20:28:58 +0200

    Form styles

--- inc/hooks.php
+++ inc/hooks.php
@@ -34,3 +34,37 @@ add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\block_editor_title_
  * Rest API hooks
  */
 require get_theme_file_path( 'inc/hooks/rest-api.php' );
+
+/**
+ * Add required attributes to Gravity Forms fields to enable native validation
+ */
+add_filter( 'gform_field_content', __NAMESPACE__ . '\add_custom_attr', 10, 5 );
+function add_custom_attr( $field_content, $field, $value, $form_id ) {
+
+  // Add type attribute to file upload button, otherwise it tries to send
+  if ( 'fileupload' === $field->type ) {
+    $field_content = str_replace( '<button', '<button type="button"', $field_content );
+  }
+
+  // Add required to get native HTML validation instead of GF jQuery version
+  if ( true === $field->isRequired ) { // phpcs:ignore
+    $field_content = str_replace( 'type=', 'required type=', $field_content );
+  }
+
+  return $field_content;
+ }
+
+/**
+ * Change gravity forms input to button that validates natively (remove onclick and onkeypress events)
+ */
+add_filter( 'gform_submit_button', __NAMESPACE__ . '\form_submit_button', 10, 2 );
+function form_submit_button( $button, $form ) {
+  return "<button type='submit' class='button gform_button' id='gform_submit_button_{$form['id']}'>Lähetä</button>";
+}
+
+/**
+* Gravity Forms Scroll To Anchor Tag
+*
+* @link https://docs.gravityforms.com/gform_confirmation_anchor/
+*/
+add_filter( 'gform_confirmation_anchor', '__return_true' );
