c19d076e31e19b1995b22f2bc6f07e2f3ca3ecfc
Roni Laukkarinen <roni@dude.fi>
2023-10-22 18:15:37 +0300

    Change to more modern dark mode toggle and move it to footer

--- footer.php
+++ footer.php
@@ -21,25 +21,43 @@ namespace Air_Light;
 
     <div class="container">
       <div class="footer-space">
-        <ul class="social-media">
-          <li><a class="no-external-link-indicator" href="https://twitter.com/rolle"><span class="screen-reader-text">Twitter</span><?php include get_theme_file_path( '/svg/twitter.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://github.com/ronilaukkarinen"><span class="screen-reader-text">GitHub</span><?php include get_theme_file_path( '/svg/github.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://www.instagram.com/rolle_"><span class="screen-reader-text">Twitter</span><?php include get_theme_file_path( '/svg/instagram.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://www.last.fm/user/rolle-"><span class="screen-reader-text">Last.fm</span><?php include get_theme_file_path( '/svg/lastfm.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://untappd.com/user/rolle"><span class="screen-reader-text">Untappd</span><?php include get_theme_file_path( '/svg/untappd.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://retroachievements.org/user/rolle"><span class="screen-reader-text">Retro Achievements</span><?php include get_theme_file_path( '/svg/joystick.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://www.overbuff.com/players/pc/Qllervo-2545"><span class="screen-reader-text">Overwatch</span><?php include get_theme_file_path( '/svg/overwatch.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://rawg.io/@rolle"><span class="screen-reader-text">Rawg.io</span><?php include get_theme_file_path( '/svg/rawg.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="http://trakt.tv/users/rolle"><span class="screen-reader-text">Trakt</span><?php include get_theme_file_path( '/svg/trakt.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://keybase.io/ronilaukkarinen"><span class="screen-reader-text">Keybase</span><?php include get_theme_file_path( '/svg/keybase.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://profiles.wordpress.org/rolle"><span class="screen-reader-text">WordPress</span><?php include get_theme_file_path( '/svg/wordpress.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://www.linkedin.com/in/rolaukka/"><span class="screen-reader-text">WordPress</span><?php include get_theme_file_path( '/svg/linkedin.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://www.strava.com/athletes/16582440"><span class="screen-reader-text">Strava</span><?php include get_theme_file_path( '/svg/strava.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" href="https://goodreads.com/rolle"><span class="screen-reader-text">Goodreads</span><?php include get_theme_file_path( '/svg/goodreads.svg' ); ?></a></li>
-          <li><a class="no-external-link-indicator" rel="me" href="https://mementomori.social/@rolle"><span class="screen-reader-text">Mastodon</span><?php include get_theme_file_path( '/svg/mastodon.svg' ); ?></a></li>
-        </ul>
+        <div class="footer-choices">
+          <ul class="social-media">
+            <li><a class="no-external-link-indicator" href="https://twitter.com/rolle"><span class="screen-reader-text">Twitter</span><?php include get_theme_file_path( '/svg/twitter.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://github.com/ronilaukkarinen"><span class="screen-reader-text">GitHub</span><?php include get_theme_file_path( '/svg/github.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://www.instagram.com/rolle_"><span class="screen-reader-text">Twitter</span><?php include get_theme_file_path( '/svg/instagram.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://www.last.fm/user/rolle-"><span class="screen-reader-text">Last.fm</span><?php include get_theme_file_path( '/svg/lastfm.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://untappd.com/user/rolle"><span class="screen-reader-text">Untappd</span><?php include get_theme_file_path( '/svg/untappd.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://retroachievements.org/user/rolle"><span class="screen-reader-text">Retro Achievements</span><?php include get_theme_file_path( '/svg/joystick.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://www.overbuff.com/players/pc/Qllervo-2545"><span class="screen-reader-text">Overwatch</span><?php include get_theme_file_path( '/svg/overwatch.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://rawg.io/@rolle"><span class="screen-reader-text">Rawg.io</span><?php include get_theme_file_path( '/svg/rawg.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="http://trakt.tv/users/rolle"><span class="screen-reader-text">Trakt</span><?php include get_theme_file_path( '/svg/trakt.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://keybase.io/ronilaukkarinen"><span class="screen-reader-text">Keybase</span><?php include get_theme_file_path( '/svg/keybase.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://profiles.wordpress.org/rolle"><span class="screen-reader-text">WordPress</span><?php include get_theme_file_path( '/svg/wordpress.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://www.linkedin.com/in/rolaukka/"><span class="screen-reader-text">WordPress</span><?php include get_theme_file_path( '/svg/linkedin.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://www.strava.com/athletes/16582440"><span class="screen-reader-text">Strava</span><?php include get_theme_file_path( '/svg/strava.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" href="https://goodreads.com/rolle"><span class="screen-reader-text">Goodreads</span><?php include get_theme_file_path( '/svg/goodreads.svg' ); ?></a></li>
+            <li><a class="no-external-link-indicator" rel="me" href="https://mementomori.social/@rolle"><span class="screen-reader-text">Mastodon</span><?php include get_theme_file_path( '/svg/mastodon.svg' ); ?></a></li>
+          </ul>
+        </div>
 
-        <p>Oikeudet omistaa Roni Laukkarinen, 1999-<?php echo esc_attr( date( 'Y' ) ); ?>.</p>
+        <div class="footer-credits">
+          <p>Oikeudet omistaa Roni Laukkarinen, 1999-<?php echo esc_attr( date( 'Y' ) ); ?>.</p>
+
+          <div role="radiogroup" aria-label="Valitse väriteema" id="dark-mode-footer-toggle" class="dark-mode-footer-toggle">
+
+            <input id="color-scheme-toggle-light" type="radio" name="colorModeToggle" value="light">
+            <label tabindex="-1" for="color-scheme-toggle-light" class="dark-mode-footer-label">Vaalea</label>
+
+            <input id="color-scheme-toggle-dark" type="radio" name="colorModeToggle" value="dark">
+            <label tabindex="-1" for="color-scheme-toggle-dark" class="dark-mode-footer-label">Tumma</label>
+
+            <input id="color-scheme-toggle-auto" type="radio" name="colorModeToggle" value="auto" checked>
+            <label tabindex="0" for="color-scheme-toggle-auto" class="dark-mode-footer-label">Automaattinen</label>
+
+          </div>
+
+        </div>
       </div>
     </div>
 
--- header.php
+++ header.php
@@ -43,7 +43,6 @@ function toggleTheme() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  checkBox = document.getElementById('toggle-dark-mode');
 
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') !== 'theme-light') {
     setTheme('theme-dark');
@@ -51,29 +50,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
   if (localStorage.getItem('theme') === 'theme-dark') {
     setTheme('theme-dark');
-    checkBox.checked = true;
+    document.getElementById('color-scheme-toggle-dark').checked = true;
+    document.getElementById('color-scheme-toggle-light').checked = false;
+    document.getElementById('color-scheme-toggle-auto').checked = false;
   } else {
     setTheme('theme-light');
-    checkBox.checked = false;
+    document.getElementById('color-scheme-toggle-dark').checked = false;
+    document.getElementById('color-scheme-toggle-light').checked = true;
+    document.getElementById('color-scheme-toggle-auto').checked = false;
   }
+
+  document.querySelectorAll('#dark-mode-footer-toggle input').forEach(function(el) {
+    el.addEventListener('change', function() {
+      if (this.value === 'light') {
+        setTheme('theme-light');
+      } else if (this.value === 'dark') {
+        setTheme('theme-dark');
+      } else {
+        localStorage.removeItem('theme');
+
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          setTheme('theme-dark');
+        } else {
+          setTheme('theme-light');
+        }
+      }
+    });
+  });
+
 });
 </script>
-
-  <form class="dark-mode-controls" onclick="toggleTheme()">
-    <label>
-      <input onclick="toggleTheme()" id="toggle-dark-mode" class="toggle-checkbox" type="checkbox"></input>
-      <div class="toggle-slot">
-        <div class="sun-icon-wrapper">
-          <svg class="sun-icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><line x1="12" y1="1" x2="12" y2="3"/><line x1="12" y1="21" x2="12" y2="23"/><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/><line x1="1" y1="12" x2="3" y2="12"/><line x1="21" y1="12" x2="23" y2="12"/><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/></svg>
-        </div>
-
-        <div class="toggle-button"></div>
-        <div class="moon-icon-wrapper">
-          <svg class="moon-icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>
-        </div>
-      </div>
-    </label>
-  </form>
 
   <a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( get_default_localization( 'Skip to content' ) ); ?></a>
   <?php wp_body_open(); ?>
